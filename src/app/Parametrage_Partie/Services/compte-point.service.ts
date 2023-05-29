6935c17b1dc543c1750502e4cad674b85afc0272
lnd <lndubourg@gmail.com>
2023-05-29 11:41:41 +0200

    attribut les points en cas de capot ou dedans

--- src/app/Parametrage_Partie/Services/compte-point.service.ts
+++ src/app/Parametrage_Partie/Services/compte-point.service.ts
@@ -46,35 +46,55 @@ export class ComptePointService {
      */
     setPreneur = (id: number) => {
         this.preneur = id;
-        console.log("le preneur est : "+this.preneur);
+        }
+
+    /**
+     *  je récupère le type de capot pour le calcul des points plus tard
+     * @param type type de capot
+     */
+    setCapot = (type: string) => {
+        this.capot = type;        
     }
 
-    setCapot = (type: string) => {
-        this.capot = type;
-        console.log("type de cpaot : "+ this.capot);
-        
+    /**
+     * attribut les points si capot ou dedans
+     * @param capot type de capot
+     * @param preneur id équipe qui a pris
+     */
+    onAddPointCapot = () => {        
+        if (this.capot == "Capot") {
+            if (this.preneur == 0) {
+                this.pointsPlieEquipe0 += 250;                
+            } else {
+                this.pointsPlieEquipe1 += 250;
+            }
+        } else if (this.capot == "Dedans") {
+            if (this.preneur == 0) {
+                this.pointsPlieEquipe1 += 160;
+            } else {
+                this.pointsPlieEquipe0 += 160;
+            }
+        }
     }
+
     /**
      * je récupère l'id de l'équipe qui va recevoir le 10 de der
      * @param id id equipe
      */
     setEquipe10Der = (id: number) => {
         this.equipe10Der = id;
-        console.log("l'équipe sélectionnée pour 10 de der est : "+this.equipe10Der);  
     }
     
     /**
      * méthode qui permet d'ajouter des points à une équipe lors de la définition de la partie
      * @param equipe10Der id de l'équipe qui reçoit les points
      */
-    onAddPoint10Der = (equipe10Der: number) => {
-        if (equipe10Der == 0) {
+    onAddPoint10Der = () => {
+        if (this.equipe10Der == 0) {
             this.pointsPlieEquipe0 += 10
-            console.log("equipe 0 resoit 10 de der " +this.pointsPlieEquipe0);
         }
-        if (equipe10Der == 1) {
+        if (this.equipe10Der == 1) {
             this.pointsPlieEquipe1 += 10
-            console.log("equipe 1 resoit 10 de der "+this._pointsEquipe1);            
         }        
     }
 
@@ -83,7 +103,12 @@ export class ComptePointService {
      * autoriser l'ajout des points du plie à chaque équipe
      */
     onAddPointTotal = () => {
-        this.onAddPoint10Der(this.equipe10Der);
+        if (this.capot == "Capot" || this.capot == "Dedans") {
+            this.onAddPointCapot();
+        } else {
+            this.onAddPoint10Der();
+        }
+        
         this._pointsEquipe0.push(this.pointsPlieEquipe0);
         this._pointsEquipe1.push(this.pointsPlieEquipe1);
         console.log("total points eq 0 " +this._pointsEquipe0);
