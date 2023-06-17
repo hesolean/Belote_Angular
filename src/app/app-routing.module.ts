aff4921683ec6c97a340159f3436e01e28c82c9a
lnd <lndubourg@gmail.com>
2023-06-17 12:06:28 +0200

    ajout de la navbar et des routing, remise en route de la page connexion mais non fonctionnelle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Login/login/login.component';
+import { AppComponent } from './app.component';
+import { PlaygroungComponent } from './playgroung/playgroung.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: PlaygroungComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
